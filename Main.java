391db40a098ab37dd731a90047f4c1912986263f
KamileKortz <kortzkamile@gmail.com>
2026-03-24 10:50:41 -0300

    xadez

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
 public class Main {
+    static final Scanner LER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
 
         Posicao lugarDoCavalo = new Posicao(1, 2);
         Cavalo meuCavalo = new Cavalo("Branco", lugarDoCavalo);
 
         System.out.println("Próxima posição do Cavalo Branco");
         System.out.print("Linha: ");
-        int linhaDigitada = teclado.nextInt();
+        int linhaDigitada = LER.nextInt();
         System.out.print("Coluna: ");
-        int colunaDigitada = teclado.nextInt();
+        int colunaDigitada = LER.nextInt();
 
         Posicao destinoDoJogador = new Posicao(linhaDigitada, colunaDigitada);
         meuCavalo.mover(destinoDoJogador);
